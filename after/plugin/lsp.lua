4f071630f679025f68a69a1d9134e3b7db441edc
Robin Janzen <rjanzen@conet.de>
2023-02-16 13:06:53 +0100

    fix: change shortcuts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -25,7 +25,8 @@ lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "M", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "K", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
 	vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
